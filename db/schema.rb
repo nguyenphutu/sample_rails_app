87026ffc59d9884ae64e1f7a5d91d3b27b9123f5
vuvantien <tien.vuvan@powergatesoftware.com>
2018-01-30 10:50:08 +0700

    pass reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118104204) do
+ActiveRecord::Schema.define(version: 20180124075957) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180118104204) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_send_at"
   end
 
 end
